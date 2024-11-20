7519b13c1446beca50ac12a9b5e54e1928045757
Yan <140942445+yano49@users.noreply.github.com>
2024-11-20 13:00:16 +0700

    Create 2.SELECT_DISTINCT_STATEMENT.sql

---
+++ w3school/2.SELECT_DISTINCT_STATEMENT.sql
@@ -0,0 +1,17 @@
+SELECT DISTINCT Country FROM Customers;
+
+--Count Distinct
+/*
+SELECT COUNT(DISTINCT Country) FROM Customers;
+
+example
+SELSECT Count(*) AS DistinctCountries
+FROM (SELECT DISTINCT Country FROM Customers);
+*/
+
+
+
+/*
+SELECT DISTICT column1, column2,...
+FROM table_name;
+*/
