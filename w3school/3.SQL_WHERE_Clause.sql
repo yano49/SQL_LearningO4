ce35e8f1345da24d9bf32885a76e8c9f6ac34972
Yan <140942445+yano49@users.noreply.github.com>
2024-11-20 13:03:09 +0700

    Create 3.SQL_WHERE_Clause.sql

---
+++ w3school/3.SQL_WHERE_Clause.sql
@@ -0,0 +1,14 @@
+SELECT * FROM Customers
+WHERE Country = 'Mexico';
+
+/*
+Example
+SELECT * FROM Customers
+WHERE CustomerID = 1;
+*/
+
+/*
+SELECT column1, column2,...
+FROM table_name
+WHERE condition;
+*/
